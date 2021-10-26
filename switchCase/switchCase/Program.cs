bc0bb6a9b6ba7f25e47a68c395b3476df88a84ee
Doğukan <60947113+Dogukan18@users.noreply.github.com>
2021-10-26 19:17:30 +0300

    Add files via upload

---
+++ switchCase/switchCase/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace switchCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //1 ile 7 arasında girilen sayının gün olarak karşılığı
+
+            /* int sayi;
+              Console.Write("1 ile 7 Arasında Bir Sayı Giriniz: ");
+              sayi = Convert.ToInt32(Console.ReadLine());
+
+              switch (sayi)
+              {
+                  case 1: Console.Write("Pazartesi");
+                      break;
+                  case 2: Console.Write("Salı");
+                      break;
+                  case 3: Console.Write("Çarşamba");
+                      break;
+                  case 4: Console.Write("Perşembe");
+                      break;
+                  case 5: Console.Write("Cuma");
+                      break;
+                  case 6: Console.Write("Perşembe");
+                      break;
+                  case 7: Console.Write("Cuma");
+                      break;
+                  default: Console.Write("Hatalı Sayı Girişi");
+                      break;
+              }
+
+              Console.ReadLine(); */
+
+            //-----------------------------------------------------------------------------------------
+
+            // mevsim girildiğinde ayları veren uygulama
+
+           /* string mevsim;
+            Console.Write("Lütfen Bir Mevsim Giriniz: ");
+            mevsim = Console.ReadLine();
+
+            switch (mevsim)
+            {
+                case "yaz": Console.WriteLine("Haziran,Temmuz,Ağustos");
+                    break;
+                case "ilkbahar": Console.WriteLine("Mart,Nisan,Mayıs");
+                    break;
+                case "kış": Console.WriteLine("Aralık,Ocak,Şubat");
+                    break;
+                case "sonbahar": Console.WriteLine("Eylül,Ekim,Kasım");
+                    break;
+                default:Console.WriteLine("Hatalı Mevsim Girişi");
+                    break;
+            }
+
+            Console.ReadLine(); */
+
+
+
+        }
+    }
+}
